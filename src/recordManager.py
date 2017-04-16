f6e9d3f36814d822f23c6c4245d8bcaa68be62e1
Paul An <hakuliu@gmail.com>
2017-04-16 01:21:58 -0700

    Added utility that separates all records into training set and testing set. This way, our testing set doesn't come from the training data, and we can be sure we do not overfit.

---
+++ src/recordManager.py
@@ -0,0 +1,29 @@
+import numpy
+
+def separateRecords(recordsFile='../ptbdb/RECORDS', outTraining='../out/trainRecords', outTesting='../out/testRecords', ratio=.75):
+    '''
+    In order to make sure we do not overfit the problem, we need to use a test set that is outside of our training set.
+    Usually, to do this, we take our data, and divide it such that some of it is used for training and some for testing
+    :return: 
+    '''
+    recFile = open(recordsFile, 'r')
+    #We could do 1 pass to figure out how many records there are, and then another pass for actually separating...
+    #But since we dont' expect the file to be that big, we'll just load it all in memory
+    recordPaths = []
+    print('reading records file...')
+    for line in recFile:
+        recordPaths.append(line.strip() + '\n')
+    print('there were a total of %d records' % len(recordPaths))
+    split = int(ratio * len(recordPaths))
+    trainSet = recordPaths[:split]
+    testSet = recordPaths[split:]
+    print('the training set will be %d records' % len(trainSet))
+    print('the test set will be %d records' % len(testSet))
+    print('writing files...')
+    o = open(outTraining, 'w')
+    o.writelines(trainSet)
+    o = open(outTesting, 'w')
+    o.writelines(testSet)
+    print('done.')
+
+separateRecords()
--- src/testData.py
+++ src/testData.py
@@ -6,21 +6,9 @@ import numpy as np
 import os
 from IPython.display import display
 
-# Demo 1 - Read a wfdb record using the 'rdsamp' function into a wfdb.Record object.
-# Plot the signals, and show the data.
-record = wfdb.rdsamp('../sampledata/a103l')
-wfdb.plotrec(record, title='Record a103l from Physionet Challenge 2015')
-display(record.__dict__)
-
 #read our own ecg data
 record = wfdb.rdsamp('../ptbdb/patient001/s0010_re')
 wfdb.plotrec(record, title='ecg data of patient 001')
 display(record.__dict__)
 
-
-# Demo 2 - Read certain channels and sections of the WFDB record using the simplified 'srdsamp' function
-# which returns a numpy array and a dictionary. Show the data.
-signals, fields=wfdb.srdsamp('../sampledata/s0010_re', channels=[14, 0, 5, 10], sampfrom=100, sampto=15000)
-display(signals)
-display(fields)
-
+print('test complete')
